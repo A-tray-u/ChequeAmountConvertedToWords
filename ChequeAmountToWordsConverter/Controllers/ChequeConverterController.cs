fca5a2a192f33980b1485f9a02141a34db56871d
Atrau Paul <AtrauS15@hotmail.com>
2020-02-27 01:38:01 +1100

    Removing the Models folder and class as I found this to not be needed. Added a comment around the null check section

--- ChequeAmountToWordsConverter/Controllers/ChequeConverterController.cs
+++ ChequeAmountToWordsConverter/Controllers/ChequeConverterController.cs
@@ -14,10 +14,12 @@ namespace ChequeAmountToWordsConverter.Controllers
         [Route("{ChequeValue}")]
         public HttpResponseMessage Get(string chequeValue)
         {
+            // Check to see if chequeValue has as value and return early if it doesnt.
             if (String.IsNullOrEmpty(chequeValue))
             {
                 return Request.CreateResponse(HttpStatusCode.BadRequest, "No value was passed to the API");
             }
+
             var logic = new Logic.ChequeConvertToWords();
 
             var result = logic.ChequeAmountToWords(chequeValue);
